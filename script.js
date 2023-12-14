e8538f959b9fda34da336ff5c8025af7c783f2d3
ReactTimee <153859878+ReactTimee@users.noreply.github.com>
2023-12-14 21:09:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const reactionButton = document.getElementById("ReactionButton");
+const reactionTimeDisplay = document.getElementById("reactionTime");
+const nameInput = document.getElementById("nameInput");
+const ageInput = document.getElementById("ageInput")
+const refreshButton = document.getElementById("refreshButton");
+let startTime, endTime, timeoutId;
+
+function startReactionTimeTest() {
+  const name = nameInput.value.trim();
+  const age = ageInput.value
+
+  if (!name || !age) {
+    alert("Ievadiet vārdu un vecumu.");
+    return;
+  }
+
+  reactionButton.style.backgroundColor = "red";
+  reactionButton.disabled = true;
+
+  const delay = getRandomDelay(1000, 5000);
+  timeoutId = setTimeout(() => {
+    changeButtonColor(name);
+  }, delay);
+}
+
+function changeButtonColor(name) {
+  age = ageInput.value
+  reactionButton.style.backgroundColor = "green";
+  startTime = new Date().getTime();
+  reactionButton.disabled = false;
+
+  reactionButton.onclick = function () {
+    endTime = new Date().getTime();
+    const reactionTime = endTime - startTime;
+
+    reactionTimeDisplay.textContent = `Reakcijas laiks: ${reactionTime} ms`;
+
+    recordReactionTime({ name, reactionTime ,age});
+
+    clearTimeout(timeoutId);
+    reactionButton.style.backgroundColor = "red";
+    reactionButton.disabled = true;
+    reactionButton.onclick = null;
+  };
+}
+
+refreshButton.addEventListener("click", function () {
+  location.reload();
+});
+
+function getRandomDelay(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+//async function recordReactionTime(data) {
+  //const baseUrl = "https://programmesana2.lv/api/rihards-db/post";
+  //const url = `${baseUrl}?name=${data.name}&reactionTime=${data.reactionTime}&age=${data.age}&key=rihards123`;
+ // //save results in db
+  //await fetch(url);
+//}
---
+++ script2.js
@@ -0,0 +1,69 @@
+const ageInput = document.getElementById('ageInput');
+const nameInput = document.getElementById('nameInput');
+const box = document.getElementById('box');
+const container = document.getElementById('game-container');
+const containerRect = container.getBoundingClientRect();
+const button = document.getElementById('startButton')
+const refreshButton = document.getElementById("refreshButton");
+
+let startTime, endTime;
+let gameRunning = false;
+
+
+button.addEventListener('click',startreact)
+ 
+function startreact(){
+  const name = nameInput.value.trim();
+  const age = ageInput.value
+
+  if (age.trim() === '' || name.trim() === '') {
+    alert('Ievadiet vārdu un vecumu');
+    ageInput.value = ''; 
+    nameInput.value = ''; 
+  } else if (!gameRunning) {
+    gameRunning = true;
+    displayBox(); 
+  }
+
+}
+ 
+function getRandomPosition(containerWidth, containerHeight) {
+  const positionX = Math.floor(Math.random() * (containerWidth - 100));
+  const positionY = Math.floor(Math.random() * (containerHeight - 100));
+  return { x: positionX, y: positionY };
+}
+
+function displayBox() {
+  const { x, y } = getRandomPosition(containerRect.width, containerRect.height);
+  startTime = new Date();
+  box.style.left = `${x}px`;
+  box.style.top = `${y}px`;
+  box.style.display = 'block';
+}
+
+function hideBox() {
+//  const name = nameInput.value.trim();
+ // const age = ageInput.value
+  if (gameRunning) {
+    endTime = new Date();
+    const reactionTime = endTime - startTime;
+    const reactionTimeDisplay = document.getElementById('reactionTimeDisplay');
+    reactionTimeDisplay.textContent = `Reakcijas laiks: ${reactionTime} ms`;
+    gameRunning = false; // Stop the game
+    document.getElementById('box').style.display = 'none';
+   // recordReactionTime({ name, reactionTime ,age});
+  }
+}
+
+refreshButton.addEventListener("click", function () {
+  location.reload();
+});
+
+document.getElementById('box').addEventListener('click', hideBox);
+
+//async function recordReactionTime(data) {
+  //const baseUrl = "https://programmesana2.lv/api/rihards-db/post";
+ // const url = `${baseUrl}?name=${data.name}&reactionTime=${data.reactionTime}&age=${data.age}&key=rihards123`;
+  //save results in db
+ // await fetch(url);
+//}
