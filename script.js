bc8fb9500369a1c3761591751a482a5f9f5a262a
ReactTimee <153859878+ReactTimee@users.noreply.github.com>
2024-01-18 21:57:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const reactionButton = document.getElementById("ReactionButton");
 const reactionTimeDisplay = document.getElementById("reactionTime");
 const nameInput = document.getElementById("nameInput");
-const ageInput = document.getElementById("ageInput")
-const refreshButton = document.getElementById("refreshButton");
+const ageInput = document.getElementById("ageInput");
+const attemptsLeftDisplay = document.getElementById("attemptsCount");
 let startTime, endTime, timeoutId;
+let testCounter = 0;
 
 function startReactionTimeTest() {
   const name = nameInput.value.trim();
-  const age = ageInput.value
+  const age = ageInput.value;
 
   if (!name || !age) {
     alert("Ievadiet vārdu un vecumu.");
@@ -24,36 +25,51 @@ function startReactionTimeTest() {
 }
 
 function changeButtonColor(name) {
-  age = ageInput.value
-  reactionButton.style.backgroundColor = "green";
-  startTime = new Date().getTime();
-  reactionButton.disabled = false;
-
-  reactionButton.onclick = function () {
-    endTime = new Date().getTime();
-    const reactionTime = endTime - startTime;
-
-    reactionTimeDisplay.textContent = `Reakcijas laiks: ${reactionTime} ms`;
-
-    recordReactionTime({ name, reactionTime ,age});
-
-    clearTimeout(timeoutId);
-    reactionButton.style.backgroundColor = "red";
-    reactionButton.disabled = true;
-    reactionButton.onclick = null;
-  };
-}
-
-refreshButton.addEventListener("click", function () {
-  location.reload();
-});
-
+    if (testCounter >= 10) {
+      return;
+    }
+    variants = 1
+    name = nameInput.value.trim();
+    age = ageInput.value;
+    reactionButton.style.backgroundColor = "green";
+    startTime = new Date().getTime();
+    reactionButton.disabled = false;
+  
+    reactionButton.onclick = function () {
+      if (testCounter >= 10) {
+        return; 
+      }
+  
+      endTime = new Date().getTime();
+      const reactionTime = endTime - startTime;
+  
+      reactionTimeDisplay.innerHTML += `<li>Reakcijas laiks ${testCounter + 1}: ${reactionTime} ms</li>`;
+  
+      testCounter++;
+      recordReactionTime({variants, name,reactionTime,age});
+      if (testCounter < 10) {
+        clearTimeout(timeoutId);
+        reactionButton.style.backgroundColor = "red";
+        reactionButton.disabled = true;
+        reactionButton.onclick = null;
+  
+        attemptsLeftDisplay.textContent = 10 - testCounter; 
+        startReactionTimeTest(); 
+       
+      } else {
+        alert("Test pabeigts. Pārbaudiet reakcijas laikus zemāk.");
+        reactionButton.disabled = true; 
+        attemptsLeftDisplay.textContent = 0;
+      }
+    };
+  }
+  
 function getRandomDelay(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 async function recordReactionTime(data) {
-  const baseUrl = "https://programmesana2.lv/api/rihards-db/post";
-  const url = `${baseUrl}?name=${data.name}&reactionTime=${data.reactionTime}&age=${data.age}&key=rihards123`;
- // //save results in db
-  await fetch(url);
-}
+    const baseUrl = "https://programmesana2.lv/api/rihards-db/post";
+    const url = `${baseUrl}?name=${data.name}&reactionTime=${data.reactionTime}&age=${data.age}&variant=${data.variants}&key=rihards123`;
+    //save results in db
+    await fetch(url);
+  }
